05f486bf98f70bd91dcbfe3125b89310959c91e2
Elvin <aoaoaoass60@gmail.com>
2026-09-02 18:47:39 +0300

    Delete palas directory

--- palas/2javacript.js
+++
@@ -1,9 +0,0 @@
-let a = 1;
-let b = 3;
-
-do {
-    a*=b;
-    b++;
-} while (b <= 49);
-
-console.log(a);
--- palas/javacript.js
+++
@@ -1,7 +0,0 @@
-let b = 5;  
-let x = -Math.PI;
-while (x < Math.PI) {
-    let y = b* Math.cos(x);
-    console.log(y);
-    x += (Math.PI - Math.PI / 8);
-}
